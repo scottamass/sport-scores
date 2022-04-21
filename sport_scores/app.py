ebf8e42b6f02580a7f931fe58af7a66388363754
scottamass <scot.atkinson@live.co.uk>
2022-04-21 14:35:44 +0100

    can now go back and forwards on dates

--- sport_scores/app.py
+++ sport_scores/app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template
-from sport_scores.data.api_requests import get_scores
+from sport_scores.data.api_requests import get_scores,date_scores
+from datetime import date, timedelta , datetime as dt
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    todays_date = date.today()
+    tomorrow= todays_date + timedelta(days=1)
+    yesterday= todays_date - timedelta(days=1)
     out=get_scores()
-    return render_template('index.html', out=out)
+    return render_template('index.html', out=out, day=todays_date, tomorrow=tomorrow, yesterday=yesterday )
+
+@app.route("/date/<date>")
+def selectday(date):
+    out=date_scores(date)
+    date_str=date
+    date_obj = dt.strptime(date_str, '%Y-%m-%d')
+    tomorrow= date_obj.date() + timedelta(days=1)
+    yesterday= date_obj.date() - timedelta(days=1)
+
+    return render_template('index.html', out=out,day=date, tomorrow=tomorrow, yesterday=yesterday)    
+
+
+@app.route("/standings")
+def standings():
+    return 
--- sport_scores/data/api_requests.py
+++ sport_scores/data/api_requests.py
@@ -8,14 +8,10 @@ def get_scores():
     
     r=requests.get("http://api.football-data.org/v2/competitions/2021/matches", headers={"X-Auth-Token":"769b4f8fdad94b5a9441ab475cdd1a8d"},params={"dateFrom":todays_date,"dateTo":todays_date}).json()
     res=r['matches']
-    for match in res:
-        hometeam = match['homeTeam']['name']
-        awayteam = match['awayTeam']['name']
-        homescore = match['score']['fullTime']['homeTeam']
-        awayscore = match['score']['fullTime']['awayTeam']
-        print("home team " + hometeam +" ")
-        print(homescore)
-        print("away team " + awayteam +" ")
-        print(awayscore)
-        print("......")
-    return res    
+    
+    return res 
+       
+def date_scores(date):
+    r=requests.get("http://api.football-data.org/v2/competitions/2021/matches", headers={"X-Auth-Token":"769b4f8fdad94b5a9441ab475cdd1a8d"},params={"dateFrom":date,"dateTo":date}).json()
+    res=r['matches']
+    return res
