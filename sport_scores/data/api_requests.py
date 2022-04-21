30de8b94617b9e411a47fc69743ccd16f0f3dfa2
scottamass <scot.atkinson@live.co.uk>
2022-04-21 11:07:53 +0100

    now pull games from todays date

--- sport_scores/data/api_requests.py
+++ sport_scores/data/api_requests.py
@@ -1,9 +1,12 @@
 
 import requests
+from datetime import date
 
 
 def get_scores():
-    r=requests.get("http://api.football-data.org/v2/competitions/2021/matches", headers={"X-Auth-Token":"769b4f8fdad94b5a9441ab475cdd1a8d"},params={"dateFrom":"2022-04-20","dateTo":"2022-04-20"}).json()
+    todays_date = date.today()
+    
+    r=requests.get("http://api.football-data.org/v2/competitions/2021/matches", headers={"X-Auth-Token":"769b4f8fdad94b5a9441ab475cdd1a8d"},params={"dateFrom":todays_date,"dateTo":todays_date}).json()
     res=r['matches']
     for match in res:
         hometeam = match['homeTeam']['name']
