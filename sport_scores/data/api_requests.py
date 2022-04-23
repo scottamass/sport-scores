2924600ea9fd8a0acbd68b0528bc49e95b0f2597
scottamass <scot.atkinson@live.co.uk>
2022-04-23 15:59:51 +0100

    refactoring

--- sport_scores/data/api_requests.py
+++ sport_scores/data/api_requests.py
@@ -8,35 +8,35 @@ with open("matches.json", "r") as read_file:
 
 match_data =data['matches']   
 
+def get_match_data():
+    with open("matches.json", "r") as read_file:
+        data = json.load(read_file)
 
-
+        match_data =data['matches'] 
+        return match_data   
 
 
 
 def get_scores():
-    with open("matches.json", "r") as read_file:
-        data = json.load(read_file)
-
-    match_data =data['matches']   
+    
     todays_date = date.today()
     strng= todays_date.strftime('%Y-%m-%d')
     print(strng)
     games =[]
-    for match in match_data:
+    for match in get_match_data():
         if match['utcDate'][0:10] == strng:
             print(match['id'])
             games.append(match)  
     return games    
 
 def date_scores(matchdate):
-    with open("matches.json", "r") as read_file:
-        data = json.load(read_file)
-
-    match_data =data['matches']   
     
-    print(matchdate)
+
+      
+    
+    
     games =[]
-    for match in match_data:
+    for match in get_match_data():
         if match['utcDate'][0:10] == matchdate:
             print(match['id'])
             games.append(match)  
